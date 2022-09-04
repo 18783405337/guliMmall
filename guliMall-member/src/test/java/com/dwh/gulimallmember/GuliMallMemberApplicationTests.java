d2391eeca15b78341fdcdcb88dfabbf2079e32b6
dwh <dowoh0826@gmail.com>
2022-09-04 15:12:11 +0800

    项目结构初始化

---
+++ guliMall-member/src/test/java/com/dwh/gulimallmember/GuliMallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwh.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guliMall-order/src/test/java/com/dwh/gulimall/gulimallorder/GuliMallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwh.gulimall.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
